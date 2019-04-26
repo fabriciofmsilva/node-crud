cbeeb135ca43305882a58905bc0ec35bd440df9c
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-04-26 15:11:53 -0300

    #1 feature (task) - Add a DAO sample

---
+++ app.js
@@ -0,0 +1,63 @@
+const AppDAO = require('./dao');
+const ProjectRepository = require('./project.repository');
+const TaskRepository = require('./task.repository');
+
+function main() {
+  const dao = new AppDAO('./database.sqlite3');
+  const blogProjectData = { name: 'Write Node.js - SQLite Tutoriral' };
+  const projectRepo = new ProjectRepository(dao);
+  const taskRepo = new TaskRepository(dao);
+  let projectId
+
+  projectRepo.createTable()
+    .then(() => taskRepo.createTable())
+    .then(() => projectRepo.create(blogProjectData.name))
+    .then((data) => {
+      console.log(data)
+      projectId = data.id;
+      const tasks = [
+        {
+          name: 'Outline',
+          description: 'High level overview of sections',
+          isComplete: 1,
+          projectId
+        },
+        {
+          name: 'Write',
+          description: 'Write article contents and code examples',
+          isComplete: 0,
+          projectId
+        }
+      ]
+
+      return Promise.all(tasks.map((task) => {
+        return taskRepo.create(task);
+      }));
+    })
+    .then(() => projectRepo.getById(projectId))
+    .then((project) => {
+      console.log(`\nRetreived project from database`)
+      console.log(`project id = ${project.id}`)
+      console.log(`project name = ${project.name}`)
+      return projectRepo.getTasks(project.id)
+    })
+    .then((tasks) => {
+      console.log('\nRetrieved project tasks from database')
+      return new Promise((resolve, reject) => {
+        tasks.forEach((task) => {
+          console.log(`task id = ${task.id}`)
+          console.log(`task name = ${task.name}`)
+          console.log(`task description = ${task.description}`)
+          console.log(`task isComplete = ${task.isComplete}`)
+          console.log(`task projectId = ${task.projectId}`)
+        })
+      })
+      resolve('success')
+    })
+    .catch((err) => {
+      console.log('Error: ')
+      console.log(JSON.stringify(err))
+    })
+}
+
+main();
---
+++ project.repository.js
@@ -0,0 +1,56 @@
+class ProjectRepository {
+  constructor(dao) {
+    this.dao = dao;
+  }
+
+  createTable() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS projects (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+      )
+    `;
+    return this.dao.run(sql);
+  }
+
+  create(name) {
+    return this.dao.run(
+      `INSERT INTO projects (name) VALUES (?)`,
+      [name]
+    );
+  }
+
+  delete(id) {
+    return this.dao.run(
+      `DELETE FROM projects WHERE id = ?`,
+      [id]
+    );
+  }
+
+  getAll() {
+    return this.dao.all(`SELECT * FROM projects`);
+  }
+
+  getById(id) {
+    return this.dao.get(
+      `SELECT * FROM projects WHERE id = ?`,
+      [id]
+    );
+  }
+
+  getTasks(projectId) {
+    return this.dao.all(
+      `SELECT * FROM tasks WHERE projectId = ?`,
+      [projectId]
+    );
+  }
+
+  update({id, name}) {
+    return this.dao.run(
+      `UPDATE projects SET name = ? WHERE id = ?`,
+      [name, id]
+    );
+  }
+}
+
+module.exports = ProjectRepository;
